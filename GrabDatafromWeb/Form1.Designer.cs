0fc1d3e6a316a7d89914bf06345e71a394db2566
Chatri Ngambenchawong <debuggingsoft@gmail.com>
2016-12-30 17:20:17 +0700

    Initial

---
+++ GrabDatafromWeb/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace GrabDatafromWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextInput = new System.Windows.Forms.RichTextBox();
+            this.btnGrab = new System.Windows.Forms.Button();
+            this.ResultGridView = new System.Windows.Forms.DataGridView();
+            this.richTextLog = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextInput
+            // 
+            this.richTextInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.richTextInput.Location = new System.Drawing.Point(12, 39);
+            this.richTextInput.Name = "richTextInput";
+            this.richTextInput.Size = new System.Drawing.Size(765, 52);
+            this.richTextInput.TabIndex = 0;
+            this.richTextInput.Text = "BEM, IFEC, PDI, PTTGC, SVOA,SAWAD, AAB\n\n";
+            // 
+            // btnGrab
+            // 
+            this.btnGrab.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.btnGrab.Location = new System.Drawing.Point(783, 39);
+            this.btnGrab.Name = "btnGrab";
+            this.btnGrab.Size = new System.Drawing.Size(87, 52);
+            this.btnGrab.TabIndex = 1;
+            this.btnGrab.Text = "Grab";
+            this.btnGrab.UseVisualStyleBackColor = true;
+            this.btnGrab.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultGridView
+            // 
+            this.ResultGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResultGridView.Location = new System.Drawing.Point(12, 126);
+            this.ResultGridView.Name = "ResultGridView";
+            this.ResultGridView.Size = new System.Drawing.Size(858, 251);
+            this.ResultGridView.TabIndex = 2;
+            // 
+            // richTextLog
+            // 
+            this.richTextLog.Location = new System.Drawing.Point(12, 410);
+            this.richTextLog.Name = "richTextLog";
+            this.richTextLog.Size = new System.Drawing.Size(858, 68);
+            this.richTextLog.TabIndex = 0;
+            this.richTextLog.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(690, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Security Code (Split with comma if you want to grab many security.)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(8, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(8, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 492);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResultGridView);
+            this.Controls.Add(this.btnGrab);
+            this.Controls.Add(this.richTextLog);
+            this.Controls.Add(this.richTextInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "DS Grab Security";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ResultGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextInput;
+        private System.Windows.Forms.Button btnGrab;
+        private System.Windows.Forms.DataGridView ResultGridView;
+        private System.Windows.Forms.RichTextBox richTextLog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GrabDatafromWeb/Form1.cs
@@ -0,0 +1,208 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrabDatafromWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        /// <summary>
+        ///     GrabWebpageData
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        private string GrabWebpageData(string url)
+        {
+            string appURL = url;
+            HttpWebRequest Request = WebRequest.Create(appURL) as HttpWebRequest;
+            HttpWebResponse Response = (HttpWebResponse)Request.GetResponse();
+
+            StreamReader srResponseReader = new StreamReader(Response.GetResponseStream(), Encoding.UTF8);
+            string strResponseData = srResponseReader.ReadToEnd();
+            srResponseReader.Close();
+            return strResponseData;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataTable dt = null;
+            Boolean AlreadyCreateHeader = false; ;
+            String output = "d://stock//";
+            String[] codes = richTextInput.Text.Split(',');
+            codes = codes.Select(x => x.TrimEnd('\r', '\n')).ToArray();
+            richTextLog.Clear();
+            for (int i = 0; i < codes.Length; i++)
+            {
+                //http://www.set.or.th/set/companyprofile.do?symbol=PDI&language=th&country=TH
+                //http://www.set.or.th/set/companyprofile.do?symbol=PDI&ssoPageId=4&language=th&country=TH
+                String webdata = GrabWebpageData("http://www.set.or.th/set/companyhighlight.do?symbol="+codes[i].Trim() +"&language=th&country=TH");
+                //Convert to Encodeing UTF8
+
+                File.WriteAllText(CheckDirectory(output) + codes[i]+ ".txt", webdata, UTF8Encoding.GetEncoding(0));
+
+                string match = "<table class=\"table table-hover table-info\">";
+
+                int  foundAt = 0;
+                //สนใจ tag table ที่เรา Grab ข้อมููลมา
+                int count_table = 0;
+                while (foundAt != -1 && count_table< 1)
+                {
+                    foundAt = webdata.IndexOf(match, foundAt + 1);
+                    if (foundAt > -1)
+                    {
+                        count_table++;
+                        webdata = webdata.Substring(foundAt);
+                        foundAt = 0+match.Length;
+                        count_table++;
+                    }
+
+                    //Console.WriteLine(foundAt);
+                }
+                if (foundAt == -1)
+                {
+                    richTextLog.AppendText("===============================\n");
+                    richTextLog.AppendText("Not Found: " + codes[i] + "\n");
+                    continue;
+                }
+
+                File.WriteAllText(CheckDirectory(output) + codes[i] + "Grab.txt", webdata, UTF8Encoding.GetEncoding(0));
+
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(webdata);
+
+                foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table"))
+                {
+                    richTextLog.AppendText("===============================\n");
+                    richTextLog.AppendText("Found: " + codes[i] + "\n");
+
+                    //HtmlNode[] nodes = table.SelectNodes("td").Where(x => x.InnerHtml.Contains("table")).ToArray();
+                    HtmlAgilityPack.HtmlDocument content = new HtmlAgilityPack.HtmlDocument();
+                    content.LoadHtml(table.InnerHtml);
+                    HtmlNode[] detail = content.DocumentNode.SelectNodes("//td").ToArray();
+                    //สร้างหัวข้อ
+                    if (!AlreadyCreateHeader)
+                    {
+                        dt = BuildTable(detail, codes);
+                        AlreadyCreateHeader = true;
+                    }
+
+                    //เอาข้อมูลมาหยอด
+                    int currentCol = dt.Columns[codes[i]].Ordinal;
+
+                    HtmlNode[] data = content.DocumentNode.SelectNodes("//tr").ToArray();
+
+                    foreach (HtmlNode entry in data)
+                    {
+                        HtmlAgilityPack.HtmlDocument trNode = new HtmlAgilityPack.HtmlDocument();
+                        trNode.LoadHtml(entry.InnerHtml);
+                        HtmlNode[] tdData = trNode.DocumentNode.SelectNodes("//td|//th").ToArray();
+                        //หา Column และยัดค่า
+                        string s = tdData[0].InnerText.Replace("&nbsp;", "");
+                        var aa = dt.AsEnumerable();
+                        DataRow foundRow = dt.Rows.Find(s);
+                        if (foundRow != null)
+                        {
+                            foundRow[currentCol] = tdData[1].InnerText.Replace("&nbsp;", "");
+                        }
+                    }
+
+                    break;
+                }
+                ResultGridView.DataSource = dt;
+                dt.TableName = codes[i];
+                dt.WriteXml(CheckDirectory(output) + codes[i] + ".xml");
+                
+            }
+        }
+
+        /// <summary>
+        ///     สร้าง Table มาเก็บผลลัพธ์
+        /// </summary>
+        /// <param name="pHeader"></param>
+        /// <param name="pSecurityCode"></param>
+        /// <returns></returns>
+        private DataTable BuildTable(HtmlNode[] pHeader, String[] pSecurityCode)
+        {
+            DataTable dt = new DataTable();
+            //Create Column 
+            for (int i = 0; i <= pSecurityCode.Length; i++)
+            {
+                if (i == 0)
+                {
+                    DataColumn column = new DataColumn();
+                    column.DataType = System.Type.GetType("System.String");
+                    column.ColumnName = "CONTENT";
+                    dt.Columns.Add(column);
+                    dt.PrimaryKey = new DataColumn[] { column };
+                }
+                else
+                {
+                    dt.Columns.Add(pSecurityCode[i - 1]);
+                }
+            }
+            //Initial Some Data
+            pHeader = pHeader.Where(x => x.GetAttributeValue("height", "0") == "15" 
+                                      || x.GetAttributeValue("height", "0") == "30").ToArray();
+
+            foreach(HtmlNode entry in pHeader)
+            {
+                DataRow rDetail = dt.NewRow();
+                rDetail[0] = entry.InnerText.Replace("&nbsp;", "");
+                dt.Rows.Add(rDetail);
+            }
+            return dt;
+        }
+
+        /// <summary>
+        ///     ตรวจว่ามี Directory ถ้าไม่มีให้สร้างใหม่เลย
+        /// </summary>
+        /// <param name="pPath"></param>
+        /// <returns></returns>
+        private String CheckDirectory(String pPath)
+        {
+            try
+            {
+                // Determine whether the directory exists.
+                if (Directory.Exists(pPath))
+                {
+                    //richTextLog.AppendText("That path exists already.");
+                    return pPath;
+                }
+
+                // Try to create the directory.
+                DirectoryInfo di = Directory.CreateDirectory(pPath);
+                //richTextLog.AppendText("The directory was created successfully at " + Directory.GetCreationTime(pPath) + ".\n");
+                return pPath;
+            }
+            catch(IOException)
+            {
+                throw;
+            }
+        }
+
+    }
+}
